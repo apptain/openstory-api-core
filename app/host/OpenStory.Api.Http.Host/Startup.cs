28e605557dceee43508e8bde8334178664c70769
jdemilo <jdemilo@clearlyagileinc.com>
2018-10-01 07:34:06 -0500

    Change launch url to root for swagger

--- app/host/OpenStory.Api.Http.Host/Startup.cs
+++ app/host/OpenStory.Api.Http.Host/Startup.cs
@@ -31,7 +31,7 @@ namespace OpenStory.Api.Http.Host
             // Register the Swagger generator, defining 1 or more Swagger documents
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+                c.SwaggerDoc("v1", new Info { Title = "OpenStory Api", Version = "v1" });
             });
         }
 
